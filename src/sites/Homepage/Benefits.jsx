fbd4979f3417c5eb969402da5a1c9f913347beb8
TerminatorMP <kingmarcel98@gmx.de>
2021-11-23 20:51:39 +0100

    benefits section + reverse

--- src/sites/Homepage/Benefits.jsx
+++ src/sites/Homepage/Benefits.jsx
@@ -4,20 +4,48 @@ import styles from "./Homepage.module.scss"
 import Section from "../../layout/Section/Section";
 import Content from "../../layout/Content/Content";
 import SectionHeading from "../../components/SectionHeading/SectionHeading";
+import  { benefitsData }  from "./homepageTextData";
+import { faTachometerAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Card = () => {
+
+const Card = ({ heading, text, icon, color, coloralpha }) => {
   return(
-      <div className={styles["benefits_card"]}>
+      <div
+          style={{"--color-card": color, "--color-card-alpha": coloralpha}}
+          className={`${styles["benefits_card"]} marginTopLarge`}
+      >
+        <div className={`${styles["heading"]} marginBottomSmall`}>
+          <FontAwesomeIcon icon={icon} />
+          <h3>{heading}</h3>
+        </div>
+        <p>
+          {text}
+        </p>
       </div>
   )
 }
+
+
 
 export default function Benefits() {
   return(
       <Section type="dark">
         <Content>
-          <SectionHeading text="Das bieten wir" />
-          <Card />
+          <div className={styles["benefits"]}>
+            <SectionHeading text="Das bieten wir" />
+            <div className={`${styles["cards"]} marginTopSmall`}>
+              {benefitsData.map((data) => {
+                return <Card
+                    heading={data.heading}
+                    text={data.text}
+                    icon={data.icon}
+                    color={data.color}
+                    coloralpha={data.color + "20"}
+                />
+              })}
+            </div>
+          </div>
         </Content>
       </Section>
   )
--- src/sites/Homepage/homepageTextData.js
+++ src/sites/Homepage/homepageTextData.js
@@ -1,18 +1,52 @@
+import {faTachometerAlt, faShieldAlt, faUser, faThumbsUp, faPaintRoller} from "@fortawesome/free-solid-svg-icons";
+
 export const homepageTextData = {
   welcome: {
     heading: 'Willkommen',
     text: 'Schön das du zu uns gefunden hast. ' +
-          'Es ist ziemlich einfach auf unserem Server spielen zu können. ' +
-          'Join unserem Teamspeak-Server komm in den Whitelist-Warteraum Dort werden wir in einem entspannten Gespräch dein Wissen zum Thema RP und unseren Serverregeln abfragen. ' +
-          'Wenn du das verfahren erfolgreich bestanden hast kann es auch schon los gehen. Du betrittst unseren Server und gestaltest deine Gesichte. Wir freuen uns auf dich!'
+        'Es ist ziemlich einfach auf unserem Server spielen zu können. ' +
+        'Join unserem Teamspeak-Server komm in den Whitelist-Warteraum Dort werden wir in einem entspannten Gespräch dein Wissen zum Thema RP und unseren Serverregeln abfragen. ' +
+        'Wenn du das verfahren erfolgreich bestanden hast kann es auch schon los gehen. Du betrittst unseren Server und gestaltest deine Gesichte. Wir freuen uns auf dich!'
   },
   unique: {
     heading: 'Was uns Ausmacht',
     text: [
-        'Roleplay an erster Stelle',
-        'Roleplay an erster Stelle',
-        'Roleplay an erster Stelle',
+      'Roleplay an erster Stelle',
+      'Roleplay an erster Stelle',
+      'Roleplay an erster Stelle',
     ]
   }
-
 }
+
+export const benefitsData = [
+    {
+      heading: "Schneller Server",
+      text: "Beste Performance von Server und Scripten garantiert ein reibungloses Spielerlebnis ",
+      icon: faTachometerAlt,
+      color: '#ff6200'
+    },
+    {
+      heading: "Anti Cheat",
+      text: "Niemand mag Hacker, deshalb bekaempfen wir sie mit voller Haerte",
+      icon: faShieldAlt,
+      color: '#0057FF'
+    },
+    {
+      heading: "Aktive Admins",
+      text: "Jeden Tag sind unsere Admins fuer eure Wuensche und Probleme online und helfen euch gern",
+      icon: faUser,
+      color: '#ff0000'
+    },
+    {
+      heading: "Einsteiger freundlich",
+      text: "Dank Guides und unserer hilfsbereiten Community wirst du dich schnell zurechtfinden ",
+      icon: faThumbsUp,
+      color: '#8400EC'
+    },
+    {
+      heading: "Du gestaltest mit",
+      text: "Wir unterstuezen euch bei euren Ideen und erweitern den Server nach euren Vorstellungen",
+      icon: faPaintRoller,
+      color: '#22F000'
+    }
+  ];
